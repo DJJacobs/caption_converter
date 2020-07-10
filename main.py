7f4a50d71421c1d522e2a5370967734f86f9c948
dj9684 <daniel.jacobs@austin.utexas.edu>
2020-07-09 19:11:50 -0500

    add file handlers

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import linecache
 import fileinput
 import sys
 import os
 import re
-import io #this is to support python 2.x
-import codecs
-import chardet
+
 
 filename = ''
 
@@ -17,14 +14,13 @@ def srt_to_txt(path,captions):
     substr = ''
     plainText = re.sub(regex, substr, strip_r)
     # print(plainText)
-    txt = open(path[0:-4]+'.txt','w')
-    txt.write(plainText)
-    txt.close()
+    with open(path[0:-4]+'.txt','w') as txt:
+        txt.write(plainText)
 
 
 def vtt_to_txt(path):
+    print('converting to TXT...')
     with open(path, 'r') as myfile: #USE THIS ONCE UPGRADE TO PYTHON 3.x
-
         text = myfile.read()
         strip_r = re.sub('\r','',text) #strip our all /r's - some vtt files have /r and /n.
         strip_vtt = re.sub('WEBVTT\n','',strip_r) #remove WEBVTT heading
@@ -32,9 +28,9 @@ def vtt_to_txt(path):
         regex = r'^\d+\:\d+\:\d+\.\d+.+\n' #vtt specific match pattern
         substr = ''
         plainText = re.sub(regex, substr, strip_1st_line,0, re.MULTILINE)
-        txt = open(path[0:-4]+'.txt','w')
-        txt.write(plainText)
-        txt.close()
+        with open(path[0:-4]+'.txt','w') as txt:
+            txt.write(plainText)
+
 
 def srt_to_vtt(path):
     print('converting SRT to VTT...')
@@ -45,12 +41,8 @@ def srt_to_vtt(path):
         captions1 = re.sub(r'^\d{1,6}\n(\d)',r'\1',text, flags=re.MULTILINE) #Remove index numbers
         captions2 = re.sub(r'(^\d\d\:\d\d\:\d\d)\,(\d\d\d)( --> \d\d\:\d\d\:\d\d)\,(\d\d\d)',r'\1.\2\3.\4',captions1, flags=re.MULTILINE)
         captions3 = re.sub(r'\A',r'WEBVTT\n\n',captions2, flags=re.MULTILINE) #remove WEBVTT from beginning
-        new_vtt_file = open(path[0:-4]+'.vtt','w')
-        new_vtt_file.write(captions3)
-        new_vtt_file.close()
-        new_vtt_file.close()
-
-
+        with open(path[0:-4]+'.vtt','w') as new_vtt_file:
+            new_vtt_file.write(captions3)
 
 
 '''
@@ -59,15 +51,13 @@ Process input and send to appropriate function
 '''
 if len(sys.argv) > 1: #check that there's at least something for an arugment.
     file_path = sys.argv[1] #set variable to path/filename
-    print("file_path: " + file_path)
     filename = os.path.basename(file_path) #set filename only variable
-    print("filename: " + filename)
     if filename.lower().endswith('.srt'): #test if srt
         print("working on srt file: " + filename)
         # utf8_converter(file_path) #strips out BOM encoding, then calls converter function
         srt_to_vtt(file_path)
     elif filename.lower().endswith('.vtt'): #test if vtt
-        print("******* coverting VTT to TXT *******")
+        print("\n******* coverting VTT to TXT *******")
         vtt_to_txt(file_path,)
 
     else:
